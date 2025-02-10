85eb29c162e1b249493773b15ce479e7c1ba8bd1
Marcellino Ornelas <marcellino.ornelas@gmail.com>
2025-02-09 22:05:49 -0800

    Add prettier and format test

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+	useTabs: false,
+	trailingComma: 'es5',
+	tabWidth: 4,
+	semi: true,
+	singleQuote: true,
+	printWidth: 80,
+	arrowParens: 'always',
+	bracketSpacing: true,
+};
+
+module.exports = config;
