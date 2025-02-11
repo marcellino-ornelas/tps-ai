cd5826cc9d61c46d3c531c6e3ff390d16a6df2ab
Marcellino Ornelas <marcellino.ornelas@gmail.com>
2025-02-10 20:16:20 -0800

    add formatting

--- prettier.config.js
+++ prettier.config.js
@@ -3,14 +3,14 @@
  * @type {import("prettier").Config}
  */
 const config = {
-	useTabs: false,
-	trailingComma: 'es5',
-	tabWidth: 4,
-	semi: true,
-	singleQuote: true,
-	printWidth: 80,
-	arrowParens: 'always',
-	bracketSpacing: true,
+    useTabs: false,
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    printWidth: 80,
+    arrowParens: 'always',
+    bracketSpacing: true,
 };
 
 module.exports = config;
